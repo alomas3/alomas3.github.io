e9daa61a80c9c98f5a17c47962481c4e7b6dfd8e
Alyssa Lomas <alyssa.lomas@snhu.edu>
2025-09-27 11:25:31 -0700

    Step 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,12 @@
+//permits reading from file
+var fs = require('fs');
+
+//trips variable contains trip information created in trips.json file
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET uncle's travel packages view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Uncles Travel Agency'});
+    res.render('travel', { title: 'Uncles Travel Agency', trips});
 };
 
 module.exports = {
