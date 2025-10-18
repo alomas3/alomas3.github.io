8e912994538a5706ed91760385a04cac06785366
alomas3 <161972617+alomas3@users.noreply.github.com>
2025-10-18 12:07:02 -0700

    Add files via upload

---
+++ Category_One_Original_Artifact/src/TopFiveDestinationList.java
@@ -0,0 +1,154 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class TopFiveDestinationList {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+            	TopDestinationListFrame topDestinationListFrame = new TopDestinationListFrame();
+                topDestinationListFrame.setTitle("Top 5 Destination List");
+                topDestinationListFrame.setVisible(true);
+            }
+        });
+    }
+}
+
+
+class TopDestinationListFrame extends JFrame {
+    private DefaultListModel listModel;
+
+    public TopDestinationListFrame() {
+        super("Top Five Destination List");
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(900, 750);
+
+        listModel = new DefaultListModel();
+
+
+        //Make updates to your top 5 list below. Import the new image files to resources directory.
+        //I have imported all 5 images and included the name of the destination along with a description
+        //Each image is also credited by the photographer
+        addDestinationNameAndPicture("1. San Francisco, California. Home of the Golden Gate Bridge and the infamous Alcatraz prison! Visit here and you will never want to leave. Photo by Justin Katz", new ImageIcon(getClass().getResource("/resources/PicSanFran.png")));
+        addDestinationNameAndPicture("2. Seattle, Washington. A beautiful lavish city with beautiful landmarks and the birthplace of Starbucks! Photo by Jeffery Hayes", new ImageIcon(getClass().getResource("/resources/PicSeattle.jpg")));
+        addDestinationNameAndPicture("3. Las Vegas, Nevada. A city that never sleeps, this wild city can create daring memories and is perfect for a get-a-way. Photo by Dietmar Rabich", new ImageIcon(getClass().getResource("/resources/PicLasVeg.jpg")));
+        addDestinationNameAndPicture("4. Los Angeles, California. Known for its famous Hollywood sign and lifestyle, come enjoy the beautiful beaches and stay a while! Photo by Thomas Wolf", new ImageIcon(getClass().getResource("/resources/PicLosAngeles.jpg")));
+        addDestinationNameAndPicture("5. Cleveland, Ohio. This beautiful city is well-known for its Rock and Roll Hall of Fame that is a hot tourist attraction! Photo by MusikAnimal", new ImageIcon(getClass().getResource("/resources/PicCleveland.jpg")));
+
+        
+        JList list = new JList(listModel);
+        JScrollPane scrollPane = new JScrollPane(list);
+
+        TextAndIconListCellRenderer renderer = new TextAndIconListCellRenderer(2);
+        //I am setting the background color to orange by modifying the JList
+        //By calling to the list and modifying its attribute of the background through the .setBackground function, it allows for a change to the background color
+        list.setBackground(Color.ORANGE);
+        //I am also setting the indivisual selection a different color when the user clicks on a specific destination
+        list.setSelectionBackground(Color.MAGENTA);
+        
+        list.setCellRenderer(renderer);
+        //I am now going to add a label with my name 
+        JLabel nameLabel = new JLabel("Developer: Alyssa Lomas");
+        //before I can make a label with my name on it I must add a location for this label otherwise it would not be able to be seen
+        getContentPane().add(nameLabel, BorderLayout.SOUTH);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void addDestinationNameAndPicture(String text, Icon icon) {
+        TextAndIcon tai = new TextAndIcon(text, icon);
+        listModel.addElement(tai);
+    }
+}
+
+
+class TextAndIcon {
+    private String text;
+    private Icon icon;
+
+    public TextAndIcon(String text, Icon icon) {
+        this.text = text;
+        this.icon = icon;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
+}
+
+
+class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer {
+    private static final Border NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
+
+    private Border insideBorder;
+
+    public TextAndIconListCellRenderer() {
+        this(0, 0, 0, 0);
+    }
+
+    public TextAndIconListCellRenderer(int padding) {
+        this(padding, padding, padding, padding);
+    }
+
+    public TextAndIconListCellRenderer(int topPadding, int rightPadding, int bottomPadding, int leftPadding) {
+        insideBorder = BorderFactory.createEmptyBorder(topPadding, leftPadding, bottomPadding, rightPadding);
+        setOpaque(true);
+    }
+
+    public Component getListCellRendererComponent(JList list, Object value,
+    int index, boolean isSelected, boolean hasFocus) {
+        // The object from the combo box model MUST be a TextAndIcon.
+        TextAndIcon tai = (TextAndIcon) value;
+
+        // Sets text and icon on 'this' JLabel.
+        setText(tai.getText());
+        setIcon(tai.getIcon());
+
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
+
+        Border outsideBorder;
+
+        if (hasFocus) {
+            outsideBorder = UIManager.getBorder("List.focusCellHighlightBorder");
+        } else {
+            outsideBorder = NO_FOCUS_BORDER;
+        }
+
+        setBorder(BorderFactory.createCompoundBorder(outsideBorder, insideBorder));
+        setComponentOrientation(list.getComponentOrientation());
+        setEnabled(list.isEnabled());
+        setFont(list.getFont());
+
+        return this;
+    }
+
+    // The following methods are overridden to be empty for performance
+    // reasons. If you want to understand better why, please read:
+    //
+    // http://java.sun.com/javase/6/docs/api/javax/swing/DefaultListCellRenderer.html#override
+
+    public void validate() {}
+    public void invalidate() {}
+    public void repaint() {}
+    public void revalidate() {}
+    public void repaint(long tm, int x, int y, int width, int height) {}
+    public void repaint(Rectangle r) {}
+}
